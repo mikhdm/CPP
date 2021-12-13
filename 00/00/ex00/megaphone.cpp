c5ce1b23aa95e554d6a1e1c0066f48d24563a10a
mikhdm <dmitry.mikhaylenko@ya.ru>
2021-12-13 03:17:41 +0300

    add ex00

--- 00/00/ex00/megaphone.cpp
+++ 00/00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 21:34:40 by rmander           #+#    #+#             */
-/*   Updated: 2021/12/11 21:39:06 by rmander          ###   ########.fr       */
+/*   Updated: 2021/12/13 03:17:02 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 int main(int argc, char *argv[])
 {
-	std::string input{};
-
-	std::cin>>input;
-
-	for (size_t i = 0; i < input.length(); ++i)
-		std::cout<<std::toupper(static_cast<int>(input[i]));
-
+	if (argc == 1)
+	{
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		return (EXIT_SUCCESS);
+	}
+	std::string curr;
+	for (int i = 1; i < argc; ++i)
+	{
+		curr = argv[i];
+		for (size_t j = 0; j < curr.length(); ++j)
+			std::cout<<static_cast<char>(std::toupper(curr[j]));
+	}
 	std::cout<<std::endl;
 	return (EXIT_SUCCESS);
 }
