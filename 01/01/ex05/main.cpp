85f7690a784fa952141d43b496eb421064fe8801
Rafael Mander <rmander@vo-c1.msk.21-school.ru>
2021-12-16 22:27:46 +0300

    add more ex05 tests & prepare ex06

--- 01/01/ex05/main.cpp
+++ 01/01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 20:26:34 by rmander           #+#    #+#             */
-/*   Updated: 2021/12/16 22:16:56 by rmander          ###   ########.fr       */
+/*   Updated: 2021/12/16 22:27:06 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,16 @@ int main(void) {
   karen.complain("DEBUGa");
   karen.complain("ERRORa");
 
-  karen.complain("INFO");
-  karen.complain("WARNING");
-  karen.complain("DEBUG");
-  karen.complain("ERROR");
+  Karen *karen2 = new Karen;
 
-  karen.complain("");
+  karen2->complain("INFO");
+  karen2->complain("WARNING");
+  karen2->complain("DEBUG");
+  karen2->complain("ERROR");
+
+  karen2->complain("");
+
+  delete karen2;
 
   return (EXIT_SUCCESS);
 }
