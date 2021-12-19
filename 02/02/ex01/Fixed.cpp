2bfc28a4f652c7e8c059367c3ae9e829edc59beb
mikhdm <dmitry.mikhaylenko@ya.ru>
2021-12-20 00:21:53 +0300

    add ex01 [initial]

---
+++ 02/02/ex01/Fixed.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 20:55:16 by rmander           #+#    #+#             */
+/*   Updated: 2021/12/20 00:17:35 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+#include <iostream>
+
+const int Fixed::_bits = BITS_COUNT;
+
+
+void Fixed::log(std::string const& message) {
+  std::cout << message << std::endl;
+}
+
+Fixed::Fixed(void) : _value(0) {
+  Fixed::log("Default constructor called");
+}
+
+
+Fixed::~Fixed(void) {
+  Fixed::log("Destructor called");
+}
+
+
+int Fixed::getRawBits(void) const {
+  log("getRawBits member function called");
+  return _value;
+}
+
+
+void Fixed::setRawBits(int const raw) {
+  _value = raw;
+}
+
+
+Fixed::Fixed(Fixed const& instance) : _value(0) {
+  Fixed::log("Copy constructor called");
+  if (this == &instance)
+    return ;
+  *this = instance;
+}
+
+
+Fixed& Fixed::operator=(Fixed const& instance) {
+  Fixed::log("Assignation operator called");
+  if (this == &instance)
+    return *this;
+  setRawBits(instance.getRawBits());
+  return *this;
+}
+
---
+++ 02/02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 20:54:55 by rmander           #+#    #+#             */
+/*   Updated: 2021/12/20 00:15:44 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+# define BITS_COUNT 8
+
+#include <string>
+
+class Fixed {
+ public:
+  Fixed(void);
+  Fixed(Fixed const& value);
+  virtual ~Fixed(void);
+  Fixed& operator=(Fixed const& rvalue); 
+
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+  static void log(std::string const& message);
+ private:
+  int _value;
+  static const int _bits;
+};
+
+
+#endif
+
