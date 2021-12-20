96631fe931ae18247f72247d3f2decf0e7539765
mikhdm <dmitry.mikhaylenko@ya.ru>
2021-12-20 03:02:10 +0300

    ex01 [in progress]

--- 02/02/ex01/Fixed.cpp
+++ 02/02/ex01/Fixed.cpp
@@ -6,23 +6,47 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 20:55:16 by rmander           #+#    #+#             */
-/*   Updated: 2021/12/20 00:22:31 by rmander          ###   ########.fr       */
+/*   Updated: 2021/12/20 02:55:36 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+#include <cmath>
 #include <iostream>
 
 const int Fixed::_bits = BITS_COUNT;
+const int Fixed::_kIntMax = FIXED_INT_MAX; 
+const int Fixed::_kIntMin = FIXED_INT_MIN;
 
 
 void Fixed::log(std::string const& message) {
   std::cout << message << std::endl;
 }
 
+
 Fixed::Fixed(void) : _value(0) {
   Fixed::log("Default constructor called");
+}
+
+
+Fixed::Fixed(int const raw) : _value(0) {
+  log("Int constructor called");
+  _value = raw << Fixed::_bits;
+}
+
+
+Fixed::Fixed(float const raw) : _value(0) {
+  log("Float constructor called");
+  _value = raw * (1 << Fixed::_bits);
+}
+
+
+Fixed::Fixed(Fixed const& instance) : _value(0) {
+  Fixed::log("Copy constructor called");
+  if (this == &instance)
+    return ;
+  *this = instance;
 }
 
 
@@ -39,14 +63,6 @@ int Fixed::getRawBits(void) const {
 
 void Fixed::setRawBits(int const raw) {
   _value = raw;
-}
-
-
-Fixed::Fixed(Fixed const& instance) : _value(0) {
-  Fixed::log("Copy constructor called");
-  if (this == &instance)
-    return ;
-  *this = instance;
 }
 
 
--- 02/02/ex01/Fixed.hpp
+++ 02/02/ex01/Fixed.hpp
@@ -6,31 +6,43 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 20:54:55 by rmander           #+#    #+#             */
-/*   Updated: 2021/12/20 00:15:44 by rmander          ###   ########.fr       */
+/*   Updated: 2021/12/20 02:44:45 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H
 # define FIXED_H
 
-# define BITS_COUNT 8
+# define BITS_COUNT     8
+# define FIXED_INT_MAX  8388607
+# define FIXED_INT_MIN -8388608   
 
 #include <string>
+#include <iostream>
 
 class Fixed {
  public:
   Fixed(void);
+  Fixed(int const raw);
+  Fixed(float const raw);
   Fixed(Fixed const& value);
   virtual ~Fixed(void);
   Fixed& operator=(Fixed const& rvalue); 
 
   int getRawBits(void) const;
   void setRawBits(int const raw);
+  float toFloat(void) const;
+  int toInt(void) const;
   static void log(std::string const& message);
  private:
   int _value;
   static const int _bits;
+  static int const _kIntMax;
+  static int const _kIntMin;
 };
+
+
+std::ostream& operator<<(std::ostream& o, Fixed const& instance);
 
 
 #endif
