4f004e0aa6c1d482adf6f7722d5c2f1ff4a71bee
mikhdm <dmitry.mikhaylenko@ya.ru>
2021-12-21 13:36:47 +0300

    add ex02

--- 02/02/ex02/main.cpp
+++ 02/02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 00:18:05 by rmander           #+#    #+#             */
-/*   Updated: 2021/12/20 23:28:52 by rmander          ###   ########.fr       */
+/*   Updated: 2021/12/21 13:35:43 by rmander          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,83 +17,120 @@
 #include <iomanip>
 
 
-void	compareDivision(Fixed x, Fixed y)
-{
+void	compareDiv(Fixed x, Fixed y) {
 	std::cout << std::setprecision(30);
-	std::cout << "Fixed:     " << std::setw(12)
+	std::cout << "Fixed:      " << std::setw(12)
 	          << x << " / " << y << " = " << x / y << std::endl;
-	std::cout << "toFloat(): " << std::setw(12)
+	std::cout << ".toFloat(): " << std::setw(12)
 	          << x.toFloat() << " / " << y.toFloat()
 	          << " = " << x.toFloat() / y.toFloat() << std::endl;
 	std::cout << std::endl;
 }
 
 
-void	compareMultiplication(Fixed fx, Fixed fy)
-{
+void	compareMult(Fixed x, Fixed y) {
 	std::cout << std::setprecision(30);
-	std::cout << "Fixed:     " << std::setw(12)
-	          << fx << " * " << fy << " = " << fx * fy << std::endl;
+	std::cout << "Fixed:      " << std::setw(12)
+	          << x << " * " << y
+						<< " = "
+						<< x * y << std::endl;
 
-	std::cout << "toFloat(): " << std::setw(12)
-	          << fx.toFloat() << " * " << fy.toFloat()
-	          << " = " << fx.toFloat() * fy.toFloat() << std::endl;
+	std::cout << ".toFloat(): " << std::setw(12)
+	          << x.toFloat() << " * " << y.toFloat()
+	          << " = "
+						<< x.toFloat() * y.toFloat() << std::endl;
 	std::cout << std::endl;
 }
 
 
-void	test()
-{
-	Fixed	x(1), y(128);
+void	compareAdd(Fixed x, Fixed y) {
+	std::cout << std::setprecision(30);
+	std::cout << "Fixed:      " << std::setw(12)
+	          << x << " + " << y
+						<< " = "
+						<< x + y << std::endl;
 
-	compareDivision(x, y);
-	x.setRawBits(1);
-	y.setRawBits(128);
-	compareDivision(x, y);
-	compareDivision(x, y);
-	x.setRawBits(127);
-	y.setRawBits(128);
-	compareDivision(127, 128);
-	compareDivision(999999, 1000000);
-	compareDivision(7777777, 8000000);
-	compareDivision(1, 1);
-	compareDivision(3, 2);
-	compareDivision(6, 2);
-
-	std::cout << "max("<<x<< ","<<y<<") = "<<Fixed::max(x, y) << std::endl;
-	std::cout << "min("<<x<< ","<<y<<") = "<<Fixed::min(x, y) << std::endl;
-
-	compareMultiplication(7777777, 0.5f);
-	compareMultiplication(7777777, 0.3f);
+	std::cout << ".toFloat(): " << std::setw(12)
+	          << x.toFloat() << " + " << y.toFloat()
+	          << " = "
+						<< x.toFloat() + y.toFloat() << std::endl;
+	std::cout << std::endl;
 }
 
-int 	main(int argc, char** argv)
-{
+void compareSub(Fixed x, Fixed y) {
+	std::cout << std::setprecision(30);
+	std::cout << "Fixed:      " << std::setw(12)
+	          << x << " - " << y
+						<< " = "
+						<< x - y << std::endl;
 
-	(void) argc;
-	(void) argv;
-	/* { */
-	/* 	test(); */
-	/* } */
-	
-	/* std::cout << SH_COLOR_BOLD */
-	/* 	<< SH_COLOR_YELLOW << "SUBJECT:" << SH_COLOR_RESET */ 
-	/* 	<<std::endl << std::endl; */
+	std::cout << ".toFloat(): " << std::setw(12)
+	          << x.toFloat() << " - " << y.toFloat()
+	          << " = "
+						<< x.toFloat() - y.toFloat() << std::endl;
+	std::cout << std::endl;
 
-	/* { */
-	/* 	Fixed a; */
-	/* 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) ); */
-	/* 	std::cout << a << std::endl; */
-	/* 	std::cout << ++a << std::endl; */
-	/* 	std::cout << a << std::endl; */
-	/* 	std::cout << a++ << std::endl; */
-	/* 	std::cout << a << std::endl; */
-	/* 	std::cout << b << std::endl; */
-	/* 	std::cout << Fixed::max(a, b) << std::endl; */
-	/* } */
-	
-	std::cout << Fixed(7777777) * Fixed(0.5f) << std::endl;
+}
 
 
-	return 0;
+int 	main(int argc, char** argv) {
+	static_cast<void>(argc);
+	static_cast<void>(argv);
+
+	{
+		Fixed	x(1), y(128);
+
+		compareDiv(x, y);
+		x.setRawBits(1);
+		y.setRawBits(128);
+		compareDiv(x, y);
+		compareDiv(x, y);
+		x.setRawBits(127);
+		y.setRawBits(128);
+		compareDiv(127, 128);
+		compareDiv(999999, 1000000);
+		compareDiv(7777777, 8000000);
+		compareDiv(1, 1);
+		compareDiv(3, 2);
+		compareDiv(6, 2);
+
+		std::cout << "max("<<x<< ", "<<y<<") = "<<Fixed::max(x, y) << std::endl;
+		std::cout << "min("<<x<< ", "<<y<<") = "<<Fixed::min(x, y)
+			<< std::endl << std::endl;
+
+		std::cout << "1 == 1 ? "<< ((Fixed(1) == Fixed(1)) ? "Yes" : "No") << std::endl;
+		std::cout << "1 < 2 ? "<< ((Fixed(1) < Fixed(2)) ? "Yes" : "No") << std::endl; 
+		std::cout << "1 > 2 ? "<< ((Fixed(1) > Fixed(2)) ? "Yes" : "No") << std::endl; 
+		std::cout << "3 > 2 ? "<< ((Fixed(3) > Fixed(2)) ? "Yes" : "No") << std::endl; 
+		std::cout << "3 != 2 ? "<< ((Fixed(3) != Fixed(2)) ? "Yes" : "No") 
+			<< std::endl << std::endl; 
+
+		compareMult(7777777, 0.5f);
+		compareMult(7777777, 0.3f);
+
+		compareAdd(1, 2);
+		compareAdd(8388606, 1);
+		compareAdd(8388606, 0.5f);
+		compareSub(1, 2);
+		compareSub(FIXED_INT_MAX, FIXED_INT_MAX);
+		compareSub(FIXED_INT_MIN, FIXED_INT_MIN);
+
+	}
+
+	std::cout << SH_COLOR_BOLD
+		<< SH_COLOR_YELLOW << "SUBJECT:" << SH_COLOR_RESET 
+		<<std::endl << std::endl;
+
+	{
+		Fixed a;
+		Fixed const b(Fixed(5.05f) * Fixed(2));
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max(a, b) << std::endl;
+	}
+	return (EXIT_SUCCESS);
 }
