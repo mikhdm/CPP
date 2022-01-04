4f5d1d611da9dddc9c7d207381c1d3c8288a9014
mikhdm <dmitry.mikhaylenko@ya.ru>
2022-01-05 01:50:10 +0300

    add animal class

---
+++ 04/04/ex00/Animal.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 23:27:58 by rmander           #+#    #+#             */
+/*   Updated: 2022/01/05 01:49:42 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+#include <iostream>
+#include <string>
+
+
+void Animal::log(std::string const& message, std::string const& color) {
+  std::cout << color << message << SH_COLOR_RESET << std::endl;
+}
+
+
+Animal::Animal(void) {
+  Animal::log("Animal created", SH_COLOR_WHITE);
+}
+
+
+Animal::Animal(Animal const& instance) {
+  if (this == &instance)
+    return ;
+  *this = instance;
+  Animal::log("Animal copy created", SH_COLOR_WHITE);
+}
+
+
+Animal::~Animal(void) {
+  Animal::log("Animal destroyed", SH_COLOR_WHITE);
+}
+
+
+Animal& Animal::operator=(Animal const& instance) {
+  if (this == &instance)
+    return *this;
+  type = instance.getType();
+  return *this;
+}
+
+
+std::string const& Animal::getType(void) const {
+  return type;
+}
+
+
+void Animal::setType(std::string const& newType) {
+  type = newType;
+}
+
+
+void Animal::makeSound(void) const {
+  Animal::log("I'm " + type, SH_COLOR_WHITE);
+}
+
---
+++ 04/04/ex00/Animal.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/04 23:28:03 by rmander           #+#    #+#             */
+/*   Updated: 2022/01/05 01:46:18 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <string>
+
+# define SH_COLOR_WHITE		"\033[37m"
+# define SH_COLOR_BLUE		"\033[34m"
+# define SH_COLOR_PURPLE	"\033[35m"
+# define SH_COLOR_RED			"\033[31m"
+# define SH_COLOR_YELLOW	"\033[33m"
+# define SH_COLOR_GREEN		"\033[32m"
+# define SH_COLOR_RESET		"\033[m"
+# define SH_COLOR_BOLD		"\033[1m"
+
+
+class Animal {
+ public:
+  Animal(void);
+  Animal(Animal const& instance);
+  virtual ~Animal(void);
+  Animal& operator=(Animal const& instance);
+
+  std::string const& getType(void) const;
+  void setType(std::string const& newType);
+  virtual void makeSound(void) const;
+  static void log(std::string const& message, std::string const& color);
+ protected:
+  std::string type;
+};
+  
+# endif // ANIMAL_HPP
