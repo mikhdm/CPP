ee794d7904f9bc2677def35a6332387af3c56468
mikhdm <dmitry.mikhaylenko@ya.ru>
2022-01-09 02:58:50 +0300

    ex01 [in progress]

---
+++ 05/05/ex01/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/06 20:53:55 by rmander           #+#    #+#             */
+/*   Updated: 2022/01/09 02:38:38 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <exception>
+
+
+class Bureaucrat {
+ public:
+  Bureaucrat(void);
+  virtual ~Bureaucrat(void);
+  Bureaucrat(Bureaucrat const& instance);
+  Bureaucrat(std::string const& name, unsigned int grade);
+  Bureaucrat& operator=(Bureaucrat const& instance);
+
+  std::string const& getName(void) const;
+  unsigned int getGrade(void) const;
+  void incGrade(void);
+  void decGrade(void);
+
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+
+  class OpOverloadException : public std::exception {
+   public:
+    explicit OpOverloadException(std::string const& message);
+    ~OpOverloadException(void) throw();
+    const char* what() const throw();
+   private:
+    std::string _message;
+  };
+
+ private:
+  static unsigned int const kGradeMax;
+  static unsigned int const kGradeMin;
+  std::string const   _name;
+  unsigned int        _grade;
+};
+
+std::ostream& operator<<(std::ostream& o, Bureaucrat const& b);
+
+#endif
---
+++ 05/05/ex01/main.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmander <rmander@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 23:06:01 by rmander           #+#    #+#             */
+/*   Updated: 2022/01/09 02:50:50 by rmander          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+#include <iostream>
+
+
+int main(void) {
+  
+  Bureaucrat president("John", 1);
+  Bureaucrat vice("David", 2);
+  Bureaucrat finlead("McLaren", 3);
+  Bureaucrat clerk("Matthew", 150);
+
+  std::cout << std::endl << "Investement bank \"Winds of the East\" staff: " << std::endl;
+  std::cout << president << std::endl;
+  std::cout << vice << std::endl;
+  std::cout << finlead << std::endl;
+  std::cout << clerk << std::endl << std::endl;;
+
+  std::cout << "Trying to increment grade for " << clerk << std::endl;
+  clerk.incGrade();
+  std::cout << clerk << std::endl;
+
+
+  std::cout << "Trying to decrement grade for " << clerk << std::endl;
+  clerk.decGrade();
+  std::cout << clerk << std::endl;
+
+  std::cout << "Trying to increment grade for " << president << std::endl;
+  try {
+    president.incGrade();
+  }
+  catch (std::exception const& e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  std::cout << "Trying to decrement grade for " << clerk << std::endl;
+  try {
+    clerk.decGrade();
+  }
+  catch (std::exception const& e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  std::cout << "Trying to create new with grade 1000" << std::endl;
+  try {
+    Bureaucrat boba("Boba", 1000);
+  }
+  catch (std::exception const& e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  std::cout << "Trying to assign to " << president << " from " << clerk << std::endl;
+  try {
+    president = clerk;
+  }
+  catch (Bureaucrat::OpOverloadException const& e) {
+    std::cout << e.what() << std::endl;
+  }
+
+  std::cout << "Trying to create clone of " << president << std::endl;
+  Bureaucrat presidenti(president);
+  std::cout << presidenti << std::endl;
+
+  return (EXIT_SUCCESS);
+}
